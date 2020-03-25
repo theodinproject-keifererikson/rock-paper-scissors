bd8fbc11419d55eb1b589806040a2473c43a7369
Keifer Erikson <keiferkode@gmail.com>
2020-03-24 18:01:54 -0600

    Completed the game() function

--- app.js
+++ app.js
@@ -1,53 +1,103 @@
+let playerScore = 0;
+let computerScore = 0;
+let roundResult;
+let roundNumber = 0;
+
 // Function that creates the Computers choice at random
 function computerPlay() {
   const result = Math.random();
 
   if (result <= 0.33) {
     return("Rock");
-  } else if (result >= 0.34 && result <= 0.66) {
+  } else if (result <= 0.66) {
     return("Paper"); 
-  } else if (result >= 0.67) {
+  } else {
     return("Scissors");
   }
 }
 
 // Function that runs computerPlay() and then creates a players choice and compares the two
 function playRound(playerSelection, computerSelection) {
-  let choice = prompt("Rock, Paper, or Scissors?");
-  computerSelection = computerPlay();
+  let choice = prompt("Round " + roundNumber + "\n\nRock, Paper, or Scissors?");
   playerSelection = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+  computerSelection = computerPlay();
 
   console.log("Player chose: " + playerSelection + " | " + "Computer chose: " + computerSelection);
-
-  if (playerSelection !== "Rock" && "Paper" && "Scissors") {
+  
+  // Error & Draw check
+  if ((playerSelection !== "Rock") && (playerSelection !== "Paper") && (playerSelection !== "Scissors")) {
+    console.log("Looks like the player made an invalid choice, awaiting a new choice...");
     alert('Please enter a valid choice. ("Rock", "Paper", or "Scissors")')
     return playRound();
   } else if (playerSelection === computerSelection) {
-    return("Draw!");
+    roundResult = "Draw!";
+    return;
   } 
+
   // Player selects Rock
   if (playerSelection == "Rock") {
     if (computerSelection == "Paper") {
-      return("You lose! Rock gets covered by Paper!");
+      computerScore++;
+      roundResult = "You lose! Rock gets covered by Paper!";
+      return;
     } else {
-      return("You win! Rock smashes Scissors!");
+      playerScore++;
+      roundResult = "You win! Rock smashes Scissors!";
+      return;
     }
   }
+
   // Player selects Paper
   if (playerSelection == "Paper") {
     if (computerSelection == "Scissors") {
-      return("You lose! Paper gets cut by Scissors!");
+      computerScore++;
+      roundResult = "You lose! Paper gets cut by Scissors!";
+      return;
     } else {
-      return("You win! Paper covers Rock!");
+      playerScore++;
+      roundResult = "You win! Paper covers Rock!";
+      return;
     }
   }
+
   // Player selects Scissors
   if (playerSelection == "Scissors") {
     if (computerSelection == "Rock") {
-      return("You lose! Scissors get smashed by Rock!");
+      computerScore++;
+      roundResult = "You lose! Scissors get smashed by Rock!";
+      return;
     } else {
-      return("You win! Scissors cut Paper!");
+      playerScore++;
+      roundResult = "You win! Scissors cut Paper!";
+      return;
     }
   }
 
 }
+
+function game() {
+  // Loop to play through 5 games
+  for (i = 1; i < 6; i++) {
+    roundNumber++;
+    // Play a round of Rock Paper Scissors
+    playRound();
+    
+    console.log(roundResult);
+
+    // Final score check
+    if (i === 5) {
+      if (playerScore > computerScore) {
+        console.log("Congratulations! You won " + playerScore + " to " + computerScore + ".");
+        return;
+      } else if (playerScore < computerScore) {
+        console.log("Sorry, it looks like you lost " + playerScore + " to " + computerScore + ".");
+        return;
+      } else {
+        console.log("Looks like it was a draw! " + playerScore + " to " + computerScore + ".");
+        return;
+      }
+    }
+  }
+}
+
+game();
